d3e3a7bd12bbb35c39b5115e597f8bad9506226c
tylergneill <tyler.g.neill@gmail.com>
2018-01-23 20:16:14 +0100

    rebrand

--- __init__.py
+++ __init__.py
@@ -1,13 +1,8 @@
-__all__ = ['settings', 'tables', 'transkrit', 'scanskrit']
+__all__ = ['tables', 'transliteration', 'scansion', 'demo_io']
 
 """
-	settings.py: manage settings for transliteration
 	tables.py: building blocks for transliteration and scansion
-	transkrit.py: transliterate individual text segments, based on settings
-	scanskrit.py: scan individual text segments, also identify them
-"""
-
-"""
-	(io.py: simple, superficial, i/o stuff just for my local-folder/command-line use)
-	(alphaskritize.py: sort a list according to sanskrit letter order)
+	transliteration.py: transliterates individual text segments
+	scansion.py: scans and metrically identifies individual text segments
+	demo_io.py: simple i/o stuff for command-line demos
 """
