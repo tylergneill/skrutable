60f1b8900e3f6bde759511b8a257fde81162250e
Tyler Neill <tyler@tylers-mbp.myfiosgateway.com>
2021-01-05 00:13:12 -0500

    begin tests

---
+++ tests/unit_tests/test_transliteration.py
@@ -0,0 +1,13 @@
+from skrutable.transliteration import Transliterator
+from skrutable import scheme_maps
+
+def test_mapping():
+
+	T = Transliterator()
+	input = "mAmakAH pARqavAS cEva kim akurvata saYjaya /\nDarmakzetre kurukzetre samavetA yuyutsavaH /"
+	T.contents = input
+	output = T.map_replace(from_scheme='SLP', to_scheme='DEV')
+	output = T.contents
+	expected_output = "मआमअकआः पआणडअवआश चऐवअ कइम अकउरवअतअ सअञजअयअ /\nधअरमअकषएतरए कउरउकषएतरए सअमअवएतआ यउयउतसअवअः /"
+
+	assert expected_output == output
