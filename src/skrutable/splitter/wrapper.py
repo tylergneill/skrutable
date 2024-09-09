8766b7e5c1e4c9df88683278d2aee0062feb05ff
Tyler Neill <tyler.g.neill@gmail.com>
2024-09-09 13:45:41 -0400

    refactor: use parallel lists rather than special chars

--- src/skrutable/splitter/wrapper.py
+++ src/skrutable/splitter/wrapper.py
@@ -1,5 +1,7 @@
 import re
 import pathlib
+from typing import List, Tuple
+
 import requests
 
 from skrutable.config import load_config_dict_from_json_file
@@ -54,7 +56,7 @@ class Splitter(object):
 
     def __init__(self):
 
-        self.punc_regex = r'[।॥\|/\\.\\?,—;!\t]+'
+        self.punc_regex = r' *[।॥\|/\\.\\?,—;!\t\n]+ *'
         self.max_char_limit = 128
         self.char_limit_split_regex_options = [r'(?<=[mṃtd]) ', r' ']
         self.ctr_splt_range = 0.8 # percentage distance measured from middle
@@ -63,8 +65,10 @@ class Splitter(object):
         self.line_count_after_split = 0
         self.token_count = 0
 
-    def get_punc(self, txt):
-        return re.findall(self.punc_regex, txt)
+    def get_sentences_and_punc(self, txt: str) -> List[str]:
+        sentences = list(filter(None, re.split(self.punc_regex, txt, flags=re.MULTILINE)))
+        punc = re.findall(self.punc_regex, txt)
+        return sentences, punc
 
     def presplit(self, txt):
         txt = txt.replace('\n', '\n##\n')
@@ -110,44 +114,46 @@ class Splitter(object):
             part_b = self.split_smart_half( txt[midpoint + 1 : ] , splt_regex_options, max_len)
             return part_a + part_b
 
-    def clean_up(self, txt, split_appearance=' '):
+    def clean_up(self, split_sentences_str: str, split_appearance=' ') -> List[str]:
         for (r_1, r_2) in [
-                ('\n_\n', ' _ '), # maintain markers for original punc
-                ('\n', ' '), # restore presplit lines
-                (' *?## ?', '\n'), # restore original newlines (sans document-final or -initial space)
-                ('-', split_appearance), # modify appearance of splits ('-', ' ', '- ', etc.)
-                ('=', ''), # QUESTION: what does this char in result even mean?
-                ('=', ''), # QUESTION: what does this char in result even mean?
-                ('(\A\s*)|(\s*\Z)', '') # string-initial and -final whitespace
-            ]:
-            txt = re.sub(r_1, r_2, txt)
-        return txt
+            ('-', split_appearance), # modify appearance of splits ('-', ' ', '- ', etc.)
+            ('=', ''), # QUESTION: what does this char in result even mean?
+            ('=', ''), # QUESTION: what does this char in result even mean?
+            ('(\A\s*)|(\s*\Z)', '') # string-initial and -final whitespace
+        ]:
+            split_sentences_str = re.sub(r_1, r_2, split_sentences_str)
+        return split_sentences_str.split('\n')
 
-    def restore_punc(self, txt, svd_pnc):
-        spl_txt = txt.split('_')
-        # print("len(svd_pnc): ", len(svd_pnc))
-        # print("txt: ", txt)
-        # print("spl_txt: ", spl_txt)
-        # print("len(spl_txt): ", len(spl_txt))
-        return ''.join([spl_txt[i] + svd_pnc[i] for i in range(len(svd_pnc))])
+    def restore_punc(self, sentences, svd_pnc):
+        return ''.join(
+            [elem for pair in zip(sentences, svd_pnc) for elem in pair]
+        )
 
-    def enforce_char_limit(self, txt):
-        old_lines = txt.split('\n')
-        new_lines = []
-        for line in old_lines:
+    def enforce_char_limit(self, txtLines) -> Tuple[List[str], List[int]]:
+        sentence_counts = []
+        new_txtLines = []
+        for i, line in enumerate(txtLines):
             if len(line) <= self.max_char_limit:
-                new_lines.append(line)
+                new_txtLines.append(line)
+                sentence_counts.append(1)
             else:
-                new_lines.append(
-                    '\n'.join(
-                        self.split_smart_half(
-                            line,
-                            self.char_limit_split_regex_options,
-                            self.max_char_limit
-                        )
+                new_txtLines.extend(
+                    parts := self.split_smart_half(
+                        line,
+                        self.char_limit_split_regex_options,
+                        self.max_char_limit
                     )
                 )
-        return '\n'.join(new_lines)
+                sentence_counts.append(len(parts))
+        return new_txtLines, sentence_counts
+
+    def restore_sentences(self, sentences, sentence_counts):
+        restored_sentences = []
+        i = 0
+        for count in sentence_counts:
+            restored_sentences.append(' '.join(sentences[i:i + count]))
+            i += count
+        return restored_sentences
 
     def count_tokens(self, text):
         tokens = re.split(r'[\n ]+', text)
@@ -165,26 +171,37 @@ class Splitter(object):
 #         orig_cwd = os.getcwd()
 #         os.chdir(wrapper_module_path)
 
-        self.line_count_before_split = len(text.split('\n'))
+        # self.line_count_before_split = len(text.split('\n'))
 
         # save original punctuation
-        if prsrv_punc:
-            saved_punc = self.get_punc(text)
+        sentences: List[str]
+        svd_punc: List[str]
+        sentences, svd_punc = self.get_sentences_and_punc(text)
 
         # prepare string for Splitter
-        text_presplit = self.presplit(text)
-        prepared_text = self.enforce_char_limit(text_presplit)
+        # text_presplit = self.presplit(text)
+        safe_sentences: List[str]
+        sent_counts: List[int]
+        safe_sentences, sent_counts = self.enforce_char_limit(sentences)
+        # prepared_text = self.enforce_char_limit(text_presplit)
 
-        self.line_count_during_split = len(prepared_text.split('\n'))
+        # self.line_count_during_split = len(prepared_text.split('\n'))
+
+        sentences_str: str = '\n'.join(safe_sentences)
 
         # post to server_splitter api
+
         if wholeFile:
             # write prepared string to Splitter input buffer and send as binary
             with open(Splitter_input_buffer_fn, 'w') as f_out:
-                f_out.write(prepared_text)
-            result = post_file(Splitter_input_buffer_fn)
+                f_out.write(sentences_str)
+            split_sentences_str = post_file(Splitter_input_buffer_fn)
         else:
-            result = post_string(prepared_text)
+            split_sentences_str = post_string(sentences_str)
+
+        split_sentences: List[str] = self.clean_up(split_sentences_str, split_appearance=' ')
+
+        restored_sentences: List[str] = self.restore_sentences(split_sentences, sent_counts)
 
         # # run Splitter
         # command = "%s/python3.5 -W ignore apply.py" % python_3_5_bin_path
@@ -195,17 +212,17 @@ class Splitter(object):
         #     result = f_in.read()
 
         # clean up results (e.g., newlines, original punctuation)
-        result = self.clean_up(result, split_appearance=' ')
-        if prsrv_punc and saved_punc != []:
-            result = self.restore_punc(result, saved_punc)
+        if prsrv_punc and svd_punc != []:
+            final_results = self.restore_punc(restored_sentences, svd_punc)
         else:
-            result = result.replace('_', ' ')
+            final_results = '\n'.join(restored_sentences).replace('_', ' ')
 
-        self.line_count_after_split = result.count('\n') + 1
 
-        self.token_count = self.count_tokens(result)
+        # self.line_count_after_split = result.count('\n') + 1
+
+        # self.token_count = self.count_tokens(result)
 
         # restore original working directory
 #         os.chdir(orig_cwd)
 
-        return result
+        return final_results
