8cd1e01b847066cd94e648b4829b9359447a9677
Tyler Neill <tyler.g.neill@gmail.com>
2024-09-10 16:07:51 -0400

    chore: improve unclear names

--- src/skrutable/splitter/wrapper.py
+++ src/skrutable/splitter/wrapper.py
@@ -5,7 +5,7 @@ from typing import List, Tuple
 from skrutable.config import load_config_dict_from_json_file
 
 config = load_config_dict_from_json_file()
-PRESERVE_PUNC_DEFAULT = config["preserve_punc_default"]
+PRESERVE_PUNCTUATION_DEFAULT = config["preserve_punctuation_default"]
 SPLITTER_INPUT_BUFFER_FN = "data/input/buffer_in.txt"
 SPLITTER_SERVER_URL = 'https://splitter-server-tylergneill.pythonanywhere.com'
 
@@ -13,8 +13,6 @@ class Splitter(object):
 
     def __init__(self):
 
-        self.punc_regex = r' *[।॥\|/\\.\\?,—;!\[\(<\t\n"][।॥\|/\\.\\?,—;!\t\n\d\[\]\(\)<>" ]*'
-        self.max_char_limit = 128
         shared_items = r'।॥\|/\\.,—;!\[(<\t\r\n"'
         self.punctuation_regex = fr' *[{shared_items}][{shared_items}\d\])> ]*'
         self.max_char_limit = {
@@ -22,58 +20,50 @@ class Splitter(object):
             "dharmamitra_2024_sept": 350,
         }
         self.char_limit_split_regex_options = [r'(?:(?:[kgtdnpbmṃḥ])) ', r'(?:(?:e[nṇ]a|asya|[ie]va|api)) ', r' ', r'a']
-        self.ctr_splt_range = 0.8 # percentage distance measured from middle
-        self.line_count_before_split = 0
-        self.line_count_during_split = 0
-        self.line_count_after_split = 0
-        self.token_count = 0
+        self.center_split_range = 0.8 # percentage distance measured from middle
 
-    def _get_sentences_and_punc(self, txt: str) -> Tuple[List[str], List[str]]:
-        sentences = list(filter(None, re.split(self.punc_regex, txt, flags=re.MULTILINE)))
-        punc = re.findall(self.punc_regex, txt)
-        return sentences, punc
     def _get_sentences_and_punctuation(self, text: str) -> Tuple[List[str], List[str]]:
         sentences = list(filter(None, re.split(self.punctuation_regex, text, flags=re.MULTILINE)))
         punctuation = re.findall(self.punctuation_regex, text)
         return sentences, punctuation
 
-    def _find_midpoint(self, txt: str, splt_regex: str) -> int:
+    def _find_midpoint(self, text: str, split_regex: str) -> int:
         """
-        Determine position of whitespace of centermost legal split of txt based on splt_regex.
+        Determine position of whitespace of centermost legal split of text based on split_regex.
         Return integer index.
         """
 
-        space_indices = [m.end()-1 for m in re.finditer(splt_regex, txt)]
-        Ds_from_mid = [abs(i - len(txt)/2) for i in space_indices] # Distances
+        space_indices = [m.end()-1 for m in re.finditer(split_regex, text)]
+        Ds_from_mid = [abs(i - len(text)/2) for i in space_indices] # Distances
         try:
             most_mid_index = space_indices[Ds_from_mid.index(min(Ds_from_mid))]
             return most_mid_index
         except ValueError:
             return 0
 
-    def _split_smart_half(self, txt: str, splt_regex_options: List[str], max_len: int) -> List[str]:
+    def _split_smart_half(self, text: str, split_regex_options: List[str], max_len: int) -> List[str]:
         """
-        Recursively split txt (string) according to splt_regex_options
+        Recursively split text (string) according to split_regex_options
             (first go for m/ṃ or t/d, otherwise any space)
         until all resulting substrings conform to max_len.
         Return list of resulting substrings.
         """
         # remove initial and final spaces
-        txt = re.sub(u"(^ *| *$)",'', txt)
-        if len(txt) <= max_len:
-            return [txt]
+        text = re.sub(u"(^ *| *$)",'', text)
+        if len(text) <= max_len:
+            return [text]
         else:
-            for splt_regex in splt_regex_options:
-                midpoint = self._find_midpoint(txt, splt_regex)
+            for split_regex in split_regex_options:
+                midpoint = self._find_midpoint(text, split_regex)
                 if (
-                    midpoint > (1.0 - (1.0 - self.ctr_splt_range) / 2) * len(txt)
+                    midpoint > (1.0 - (1.0 - self.center_split_range) / 2) * len(text)
                     or
-                    midpoint < ((1.0 - self.ctr_splt_range) / 2) * len(txt)
+                    midpoint < ((1.0 - self.center_split_range) / 2) * len(text)
                 ): continue
                 else: break
 
-            part_a = self._split_smart_half( txt[ : midpoint + 1] , splt_regex_options, max_len)
-            part_b = self._split_smart_half( txt[midpoint + 1 : ] , splt_regex_options, max_len)
+            part_a = self._split_smart_half( text[ : midpoint + 1] , split_regex_options, max_len)
+            part_b = self._split_smart_half( text[midpoint + 1 : ] , split_regex_options, max_len)
             return part_a + part_b
 
     def _enforce_char_limit(self, text_lines: List[str], max_char_limit: int=128) -> Tuple[List[str], List[int]]:
@@ -159,22 +149,22 @@ class Splitter(object):
             i += count
         return restored_sentences
 
-    def _restore_punc(self, sentences: List[str], svd_pnc: List[str]) -> str:
-        if len(svd_pnc) == len(sentences) + 1: # started with "punctuation"
-            return svd_pnc[0] + ''.join(
-                [elem for pair in zip(sentences, svd_pnc[1:]) for elem in pair]
+    def _restore_punctuation(self, sentences: List[str], punctuation: List[str]) -> str:
+        if len(punctuation) == len(sentences) + 1: # started with punctuation
+            return punctuation[0] + ''.join(
+                [elem for pair in zip(sentences, punctuation[1:]) for elem in pair]
             )
-        elif len(svd_pnc) == len(sentences):
+        elif len(punctuation) == len(sentences):
             return ''.join(
-                [elem for pair in zip(sentences, svd_pnc) for elem in pair]
+                [elem for pair in zip(sentences, punctuation) for elem in pair]
             )
 
     def split(
             self,
             text: str,
             splitter_model: str='dharmamitra_2024_sept',
-            prsrv_punc: bool=PRESERVE_PUNC_DEFAULT,
-            wholeFile: bool=False,
+            preserve_punctuation: bool=PRESERVE_PUNCTUATION_DEFAULT,
+            whole_file: bool=False,
     ) -> str:
         """
         Splits sandhi and compounds of multi-line Sanskrit string,
@@ -183,8 +173,8 @@ class Splitter(object):
         """
         # save original punctuation
         sentences: List[str]
-        svd_punc: List[str]
-        sentences, svd_punc = self._get_sentences_and_punc(text)
+        saved_punctuation: List[str]
+        sentences, saved_punctuation = self._get_sentences_and_punctuation(text)
         if len(saved_punctuation) - len(sentences) > 1:
             raise ValueError("Punctuation and sentence count mismatch")
 
@@ -204,7 +194,7 @@ class Splitter(object):
 
             split_sentences_str: str
 
-            if wholeFile:
+            if whole_file:
                 # write prepared string to Splitter input buffer and send as binary
                 with open(SPLITTER_INPUT_BUFFER_FN, 'w') as f_out:
                     f_out.write(sentences_str)
@@ -221,8 +211,8 @@ class Splitter(object):
         restored_sentences: List[str] = self._restore_sentences(split_sentences, sent_counts)
 
         # restore punctuation
-        if prsrv_punc and svd_punc != []:
-            final_results = self._restore_punc(restored_sentences, svd_punc)
+        if preserve_punctuation and saved_punctuation != []:
+            final_results = self._restore_punctuation(restored_sentences, saved_punctuation)
         else:
             final_results = '\n'.join(restored_sentences).replace('_', ' ')
 
