fa5979ea5650603728e6cba07dc771b495a83be4
Tyler Neill <tyler.g.neill@gmail.com>
2024-10-18 23:36:52 -0400

    feat: don't rely on <text>

--- src/skrutable/xml_utils.py
+++ src/skrutable/xml_utils.py
@@ -4,7 +4,7 @@ from collections import deque
 
 
 def get_starting_el(xml_string_input: str) -> Tuple[Optional[etree._Element], Optional[etree._Element]]:
-    parser = etree.XMLParser(ns_clean=True, remove_comments=True)
+    parser = etree.XMLParser()
     root_el = etree.fromstring(xml_string_input, parser)
 
     # skip <teiHeader> and go straight to <text> element
@@ -17,11 +17,13 @@ def extract_text_from_tei_xml(xml_string_input: str) -> Tuple[str, List[int]]:
     Extracts .text and .tail string textual content from TEI XML <text>.
     Also keeps count of lines for each text block (relevant when restoring in case of splitting).
     """
-    _, starting_el = get_starting_el(xml_string_input)
+    root_el, text_el = get_starting_el(xml_string_input)
 
-    if starting_el is None:
-        print("Warning: Could not find starting element in XML.")
+    if root_el is None:
+        print("Warning: Could not find root element in XML.")
         return [], []
+
+    starting_el = text_el if text_el is not None else root_el
 
     texts_to_transform = []
     text_line_counts = []
@@ -44,17 +46,19 @@ def restore_tei_xml(original_xml_string_input: str, transformed_text_str: str, t
     """
     Puts transformed .text and .tail string textual content back in place.
     """
+    root_el, text_el = get_starting_el(original_xml_string_input)
+
+    if root_el is None:
+        print("Warning: Could not find root element in XML.")
+        return ""
+
+    starting_el = text_el if text_el is not None else root_el
+
     transformed_texts = transformed_text_str.split('\n')
 
     # ensure that number of lines is correct post-transformation before proceeding
     if len(transformed_texts) != sum(text_line_counts):
         raise ValueError("transformed_texts and text_line_counts count mismatch")
-
-    root_el, starting_el = get_starting_el(original_xml_string_input)
-
-    if root_el is None or starting_el is None:
-        print("Warning: Could not find root or starting element in XML.")
-        return ""
 
     # switch to double-ended queues before consumption for more efficient pop from front
     transformed_texts_dq = deque(transformed_texts)
