0386329a19d211e96dc387e355ee9c6555dac7c6
Tyler Neill <tyler.g.neill@gmail.com>
2024-12-12 15:07:08 -0500

    fix: make encoding explicit on config load

--- src/skrutable/config.py
+++ src/skrutable/config.py
@@ -4,6 +4,6 @@ import json
 def load_config_dict_from_json_file():
 	abs_dir = os.path.split(os.path.abspath(__file__))[0]
 	settings_file_path = os.path.join(abs_dir, 'config.json')
-	config_data = open(settings_file_path,'r').read()
+	config_data = open(settings_file_path, 'r', encoding='utf-8').read()
 	config = json.loads(config_data)
 	return config
