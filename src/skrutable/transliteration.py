1e3e4e0d2b8b163c3f3b18b603ecd1c52a78c881
Tyler Neill <tyler.g.neill@gmail.com>
2026-04-09 18:51:10 -0400

    fix: use conditional normalization for Indic scripts too

--- src/skrutable/transliteration.py
+++ src/skrutable/transliteration.py
@@ -144,7 +144,7 @@ class Transliterator():
 		- For IAST, ISO 15919, ITRANS when preserve_anunasika is False
 		"""
 		always_normalize = ['HK', 'VH', 'WX', 'IASTREDUCED']
-		conditionally_normalize = ['IAST', 'ITRANS', 'SLP']
+		conditionally_normalize = ['IAST', 'ITRANS', 'SLP', 'DEV', 'BENGALI', 'GUJARATI']
 		if to_scheme in always_normalize or (to_scheme in conditionally_normalize and not preserve_anunasika):
 			self.contents = self.contents.replace('~', 'M')
 
